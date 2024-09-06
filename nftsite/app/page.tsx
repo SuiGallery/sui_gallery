8226243aa2e459109d17ee6bf5745923251d2abb
Mindfrog <cyberchenjw@gmail.com>
2024-09-06 22:57:49 +0800

    [fix] nftsite

--- nftsite/app/page.tsx
+++ nftsite/app/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+import { ConnectButton } from "@mysten/dapp-kit";
+
 export default function Home() {
   return (
     <div className="">
-      
+      <ConnectButton></ConnectButton>
     </div>
   );
 }
